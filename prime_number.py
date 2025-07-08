3e9643c1b94b7903653111d9752ac247ac1d3d06
Hemasundari-yenda <yendahema@gmail.com>
2025-07-08 09:20:56 +0530

    Add files via upload

---
+++ prime_number.py
@@ -0,0 +1,17 @@
+num=int(input('enter the value'))
+if num <=1:
+    print('this not a prime number')
+else:
+    for i in range(2,int(num**0.5)+1):
+        if num%i==0:
+            print("the number is not a prime number")
+            break
+    else:
+        print('this is a prime number')
+       
+        
+
+   
+    
+
+
